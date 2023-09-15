9899d82c759b9b387482287735f912a444de5f5c
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2023-09-15 20:45:48 +0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,156 @@
+use nannou::prelude::*;
+
+// DEFAULT PARAMS
+// TURNFACTOR: Controls how much boids can turn in response to their neighbors.
+const TURNFACTOR: f32 = 0.2;
+
+// VISUAL_RANGE: Defines the maximum distance at which boids can detect and react to other boids.
+const VISUAL_RANGE: f32 = 40.0;
+
+// PROTECTED_RANGE: Specifies the minimum distance to maintain from other boids to avoid collisions.
+const PROTECTED_RANGE: f32 = 8.0;
+
+// CENTERING_FACTOR: Determines how strongly boids are attracted to the center of the flock.
+const CENTERING_FACTOR: f32 = 0.0005;
+
+// AVOID_FACTOR: Controls how much boids avoid collisions with each other.
+const AVOID_FACTOR: f32 = 0.005;
+
+// MATCHING_FACTOR: Controls how much boids attempt to match the velocity of their neighbors.
+const MATCHING_FACTOR: f32 = 0.05;
+
+// MAX_SPEED: Sets the maximum speed at which boids can move.
+const MAX_SPEED: f32 = 6.0;
+
+// MIN_SPEED: Sets the minimum speed below which boids won't slow down further.
+const MIN_SPEED: f32 = 3.0;
+
+// MAX_BIAS: Specifies the maximum bias (a parameter for behavioral variation) for boids.
+const MAX_BIAS: f32 = 0.01;
+
+// BIAS_INCREMENT: Determines the rate at which bias values change over time.
+const BIAS_INCREMENT: f32 = 0.00004;
+
+// DEFAULT_BIAS_VAL: Sets the initial bias value for boids.
+const DEFAULT_BIAS_VAL: f32 = 0.001;
+
+struct Params {
+    turnfactor: f32,
+    visual_range: f32,
+    protected_range: f32,
+    centering_factor: f32,
+    avoid_factor: f32,
+    matching_factor: f32,
+    max_speed: f32,
+    min_speed: f32,
+    max_bias: f32,
+    bias_increment: f32,
+    default_bias_val: f32,
+}
+
+struct Boid {
+    position: Vec2,
+    velocity: Vec2,
+    direction_x: DirectionX,
+    direction_y: DirectionY,
+}
+
+fn main() {
+    nannou::app(model).update(update).view(view).run();
+}
+
+enum DirectionX {
+    Right,
+    Left,
+}
+
+enum DirectionY {
+    Top,
+    Bottom,
+}
+
+struct Model {
+    _boids: Vec<Boid>,
+    _window: WindowId,
+    _params: Params,
+}
+
+fn model(_app: &App) -> Model {
+    let window = _app.new_window().view(view).build().unwrap();
+    let position = pt2(-100.0, 100.0);
+    let velocity = pt2(4.0, 3.0);
+
+    let params = Params {
+        turnfactor: TURNFACTOR,
+        avoid_factor: AVOID_FACTOR,
+        bias_increment: BIAS_INCREMENT,
+        centering_factor: CENTERING_FACTOR,
+        default_bias_val: DEFAULT_BIAS_VAL,
+        visual_range: VISUAL_RANGE,
+        protected_range: PROTECTED_RANGE,
+        matching_factor: MATCHING_FACTOR,
+        max_speed: MAX_SPEED,
+        min_speed: MIN_SPEED,
+        max_bias: MAX_BIAS,
+    };
+
+    let mut boids = Vec::new();
+
+    for i in 0..10 {
+        let boid = Boid {
+            position: pt2(position[0] + i as f32 * 10.0, position[1] + i as f32 * 30.0),
+            velocity,
+            direction_x: DirectionX::Right,
+            direction_y: DirectionY::Top,
+        };
+
+        boids.push(boid);
+    }
+
+    Model {
+        _boids: boids,
+        _window: window,
+        _params: params,
+    }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+    let boundary = _app.window_rect();
+    let boids = &mut _model._boids;
+
+    for boid in boids {
+        if boid.position[0] >= boundary.right() - 10.0 {
+            boid.direction_x = DirectionX::Left;
+        } else if boid.position[0] <= boundary.left() + 10.0 {
+            boid.direction_x = DirectionX::Right;
+        }
+
+        if boid.position[1] >= boundary.top() - 10.0 {
+            boid.direction_y = DirectionY::Bottom;
+        } else if boid.position[1] <= boundary.bottom() + 10.0 {
+            boid.direction_y = DirectionY::Top;
+        }
+
+        match boid.direction_x {
+            DirectionX::Left => boid.position[0] -= boid.velocity[0],
+            DirectionX::Right => boid.position[0] += boid.velocity[0],
+        }
+
+        match boid.direction_y {
+            DirectionY::Top => boid.position[1] += boid.velocity[1],
+            DirectionY::Bottom => boid.position[1] -= boid.velocity[1],
+        }
+    }
+}
+
+fn view(_app: &App, _model: &Model, frame: Frame) {
+    let draw = _app.draw();
+    draw.background().color(WHITE);
+
+    for boid in &_model._boids {
+        let position = boid.position;
+        draw.ellipse().xy(position).w_h(20.0, 20.0).color(BLACK);
+    }
+
+    draw.to_frame(_app, &frame).unwrap();
+}
